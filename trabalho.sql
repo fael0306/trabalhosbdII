d7d493fe0d5c9bb9ce2e86dfc9e15f80bd526a89
Manteiga <rafael.manteiga@hotmail.com>
2024-04-26 01:01:15 -0300

    Create trabalho.sql

---
+++ trabalho.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Documento(
+ Titulo VARCHAR(50),  
+ Datas DATE,  
+ Autor VARCHAR(50),  
+ Conteudo VARCHAR(50)
+);
+
+INSERT INTO Documento VALUES ('Teste', '03/06/1999', 'Autor', 'conteudo');
