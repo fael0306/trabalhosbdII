316a2988bb947b71d64d29926e343df4e1394c54
Manteiga <rafael.manteiga@hotmail.com>
2024-04-27 17:24:21 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -5,4 +5,12 @@ CREATE TABLE Documento(
  Conteudo VARCHAR(50)
 );
 
-INSERT INTO Documento VALUES ('Teste', '03/06/1999', 'Autor', 'conteudo');
+CREATE TABLE Acervo(
+id INT PRIMARY KEY,
+nome VARCHAR(100),
+descricao TEXT,
+data_criacao DATE);
+
+ALTER TABLE documento
+ADD COLUMN acervo_id INT,
+ADD FOREIGN KEY (acervo_id) REFERENCES Acervo(id);
