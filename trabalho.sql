87c3bfec3eaa99212e6d81720191ab63233a4272
Manteiga <rafael.manteiga@hotmail.com>
2024-04-27 18:21:34 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -4,22 +4,26 @@ CREATE TABLE Documento(
   Autor VARCHAR(50), 
   Conteudo VARCHAR(50)
 );
+
 CREATE TABLE Acervo(
   id INT PRIMARY KEY, 
   tema VARCHAR(100), 
   instituicao VARCHAR(100)
 );
+
 ALTER TABLE 
   documento 
 ADD 
   COLUMN acervo_id INT, 
 ADD 
   FOREIGN KEY (acervo_id) REFERENCES Acervo(id);
+
 CREATE TABLE pesquisador(
   nome VARCHAR(50), 
   afiliacao VARCHAR(100), 
   matricula VARCHAR(30) PRIMARY KEY
 );
+
 CREATE TABLE Pesquisador_Acervo(
   pesquisador_matricula VARCHAR(30) REFERENCES Pesquisador(matricula), 
   acervo_id INT REFERENCES Acervo(id), 
