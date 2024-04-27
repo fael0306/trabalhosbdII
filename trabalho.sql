7b94328589c89349e4333453b771a73548ac1dc7
Manteiga <rafael.manteiga@hotmail.com>
2024-04-27 18:20:01 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -1,28 +1,29 @@
 CREATE TABLE Documento(
- Titulo VARCHAR(50),  
- Datas DATE,  
- Autor VARCHAR(50),  
- Conteudo VARCHAR(50)
+  Titulo VARCHAR(50), 
+  Datas DATE, 
+  Autor VARCHAR(50), 
+  Conteudo VARCHAR(50)
 );
-
 CREATE TABLE Acervo(
-    id INT PRIMARY KEY,
-    tema VARCHAR(100),
-    instituicao VARCHAR(100)
+  id INT PRIMARY KEY, 
+  tema VARCHAR(100), 
+  instituicao VARCHAR(100)
 );
-
-ALTER TABLE documento
-ADD COLUMN acervo_id INT,
-ADD FOREIGN KEY (acervo_id) REFERENCES Acervo(id);
-
+ALTER TABLE 
+  documento 
+ADD 
+  COLUMN acervo_id INT, 
+ADD 
+  FOREIGN KEY (acervo_id) REFERENCES Acervo(id);
 CREATE TABLE pesquisador(
- nome VARCHAR(50),
- afiliacao VARCHAR(100),
- matricula VARCHAR(30) PRIMARY KEY
+  nome VARCHAR(50), 
+  afiliacao VARCHAR(100), 
+  matricula VARCHAR(30) PRIMARY KEY
 );
-
 CREATE TABLE Pesquisador_Acervo(
-pesquisador_matricula VARCHAR(30) REFERENCES Pesquisador(matricula),
-acervo_id INT REFERENCES Acervo(id),
-PRIMARY KEY (pesquisador_matricula, acervo_id)
- );
+  pesquisador_matricula VARCHAR(30) REFERENCES Pesquisador(matricula), 
+  acervo_id INT REFERENCES Acervo(id), 
+  PRIMARY KEY (
+    pesquisador_matricula, acervo_id
+  )
+);
