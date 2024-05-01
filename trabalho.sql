3bdd47ca3c727b4b28fc8a944a7ef548c0c563aa
Manteiga <rafael.manteiga@hotmail.com>
2024-05-01 00:05:42 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -61,7 +61,7 @@ VALUES
   (
     'Teoria y práctica del psicoanálisis', 
     '01/02/1926', 'Emilio Mira y López', 
-    'Apresenta uma visão geral da psicanálise freudiana.', 
+    'Visão geral da psicanálise freudiana.', 
     1
   );
   
@@ -82,7 +82,7 @@ INSERT INTO Documento(
 VALUES 
   (
     'Manual de Psiquiatria', '03/06/1935', 
-    'Emilio Mira y Lopez', 'Fornece uma visão dos transtornos mentais da época.', 
+    'Emilio Mira y Lopez', 'Percepção dos transtornos mentais da época.', 
     1
   );
   
@@ -97,7 +97,7 @@ VALUES
     1
   );
   
-INSERT INTO documento(
+INSERT INTO Documento(
   Titulo, Datas, Autor, Conteudo, acervo_id
 ) 
 VALUES 
@@ -105,6 +105,106 @@ VALUES
     'Cuatro gigantes del alma', '09/08/1959', 
     'Emilio Mira y Lopez', 'Explora as 4 emoções básicas.', 
     1
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Estudos de Psicologia Social', '23/05/2015', 
+    'Celso Pereira de Sá', 'Manual sobre vertentes da Psicologia Social.', 
+    2
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Pesquisa em Representações Sociais', '03/09/1998', 
+    'Celso Pereira de Sá', 'Metodologia em representações sociais.', 
+    2
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Memórias do Descobrimento do Brasil', '25/05/2005', 
+    'Celso Pereira de Sá', 'Disserta sobre a história do Brasil.', 
+    2
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Psicologia do Controle Social', '03/09/1973', 
+    'Celso Pereira de Sá', 'Perspectivas de questões sociológicas na sociologia.', 
+    2
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Sobre a Psicologia Social no Brasil', '21/10/2007', 
+    'Celso Pereira de Sá', 'História da Psicologia Social no Brasil.', 
+    2
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Orientação Infantil', '09/03/1988', 
+    'Isabel Adrados', 'Explanação sobre questões da infância.', 
+    3
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'A Técnica de Rorschach em crianças', '08/04/1985', 
+    'Isabel Adrados', 'Manual de aplicação de testes projetivos em crianças.', 
+    3
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Teoria e Prática do Teste de Rorschach', '09/11/1982', 
+    'Isabel Adrados', 'Manual teórico de testes projetivos em crianças.', 
+    3
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Rorschach na adolescência normal e patológica', '07/06/1976', 
+    'Isabel Adrados', 'Aplicação de Rorschach na adolescência.', 
+    3
+  );
+
+INSERT INTO Documento(
+  Titulo, Datas, Autor, Conteudo, acervo_id
+) 
+VALUES 
+  (
+    'Manual de Psicodiagnóstico', '08/08/1982', 
+    'Isabel Adrados', 'Instrução da atuação do psicólogo frente a diagnósticos.', 
+    3
   );
 
 /*
