8f2f6653b99a166f9c060fb907e1ddb5770f736d
Manteiga <rafael.manteiga@hotmail.com>
2024-06-25 23:21:40 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -394,7 +394,8 @@ FROM
 
 -- Triggers
 CREATE 
-OR REPLACE FUNCTION validar_data() RETURNS TRIGGER AS $$ BEGIN IF NEW.datas > CURRENT_DATE THEN RAISE EXCEPTION 'A data do documento não pode ser posterior à data atual.';
+OR REPLACE FUNCTION validar_data() RETURNS TRIGGER AS $$ BEGIN 
+IF NEW.datas > CURRENT_DATE THEN RAISE EXCEPTION 'A data do documento não pode ser posterior à data atual.';
 END IF;
 RETURN NEW;
 END;
